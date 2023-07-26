dee903b89f6247bbb228aa1069807d00df29956c
Lucas N <98057712+Luccxx@users.noreply.github.com>
2023-07-26 18:51:49 -0300

    Adding new style to header.js

--- view/header.js
+++ view/header.js
@@ -1,5 +1,19 @@
 document.write(
-    `<div id="header-box-container">
+    `
+    <style>
+        body{
+        }
+        #header-box-container{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: gray;
+            font-size: 45px;
+            text-align: center;
+            padding: 1rem;
+            margin-bottom: 10px;
+        }
+    </style>
+
+    <div id="header-box-container">
         <header>Movies-Extreme</header>
     </div>
     `
