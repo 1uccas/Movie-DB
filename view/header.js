d247f81cd1ace10b0747d3674aa974a2191401ea
Lucas N <98057712+Luccxx@users.noreply.github.com>
2023-07-27 15:13:31 -0300

    Updating header font, name and mascot

--- view/header.js
+++ view/header.js
@@ -1,14 +1,16 @@
 document.write(
     `
     <style>
+    @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Dancing+Script:wght@700&family=Fruktur&family=Give+You+Glory&family=Lobster&family=Nanum+Pen+Script&family=Oswald:wght@300;400&family=Patrick+Hand&family=Roboto:wght@300&family=Shadows+Into+Light&family=Sriracha&display=swap');
+  
         body{
             padding: 0;
             margin: 0;
         }
         #header-box-container{
-            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+            font-family: 'Lobster', cursive;
             background: linear-gradient(to right, gray, white); 
-            font-size: 45px;
+            font-size: 50px;
             text-align: center;
             padding: 1rem;
             margin-bottom: 30px;
@@ -20,14 +22,14 @@ document.write(
         }
         #header-box-container img{
             position: absolute;
-            margin-top: -10px;
+            margin-top: -5px;
             margin-left: -75px;
             width: 70px;
         }
     </style>
 
     <div id="header-box-container">
-        <header><img src="/image/header-image/camaleao.png"></img> Movies-Extreme</header>
+        <header><img src="/image/header-image/box.png"></img> Actors Box</header>
     </div>
     `
 )
